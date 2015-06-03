553922e78b3b2a15907f44746848a6d167a3a7b2
John Doe <msi.moussi@gmail.com>
2015-06-03 17:18:54 +0100

    CRUD of USERS with JQUERY validation and redirection

--- www/js/manage_user.js
+++ www/js/manage_user.js
@@ -15,7 +15,13 @@ $("document").ready(function(){
       url: "http://127.0.0.1:8880/e_advRes/www/server/manage_user.php", //Relative or absolute path to response.php file
       data: data,
       success: function(data) {
-        alert(JSON.stringify(data));
+      	var d = $.parseJSON(JSON.stringify(data)).response;
+      	alert(d);
+
+				if (d == "Successfully Registered!") {
+
+					window.location.replace("authen.html");
+				}
 
        // alert("Form submitted successfully.\nReturned json: " + data["json"]);
       }
@@ -58,45 +64,6 @@ function getXMLHttp()
   return xmlHttp;
 }
 
-function doSaveUser(){
-	 // Create our XMLHttpRequest object
-    updReq = getXMLHttp();
-    // Create some variables we need to send to our PHP file
-    var url = "http://127.0.0.1:8880/e_advRes/www/server/manage_user.php";
-    
-    var name = document.getElementById("name").value;
-    
-    var surname = document.getElementById("surname").value;
-    
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    	
-    
-    var vars = "name="+name+"&surname="+surname+"&email="+email+"&password="+password;
-    
-	updReq.open('POST', url, true);
-        updReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-       updReq.setRequestHeader("Access-Control-Allow-Origin", "*");
-  
-      
-        updReq.onreadystatechange = function() {//Call a function when the state changes.
-                if(updReq.readyState == 4 && updReq.status == 200) {
-                        HandleResponse(updReq.responseText);
-                        
-                     //   window.location.replace("authen.html");
-                }
-        }
-        updReq.send(vars);
-
-}
-
-
-
-// HandleResponse
-function HandleResponse(response)
-{
-  document.getElementById('ResponseDiv').innerHTML = response;
-}
 
 
 
