5d2d0caf82e6d58478fadbce86619799da6c1c01
John Doe <msi.moussi@gmail.com>
2015-10-05 18:07:04 +0100

    adding affect product to chef and region to representatif

--- www/server/deleteProduct.php
+++ www/server/deleteProduct.php
@@ -1,6 +1,6 @@
-<?php
-    header('Access-Control-Allow-Origin: *');
-header("Content-Type: application/json; charset=UTF-8");
+﻿<?php
+ //   header('Access-Control-Allow-Origin: *');
+//header("Content-Type: application/json; charset=UTF-8");
 $id_Produit=($_POST['index']);
 
 	mysql_connect("10.0.210.173", "moha","moha") or die(mysql_error()); //Connect to server
@@ -8,7 +8,7 @@ $id_Produit=($_POST['index']);
 	
 	$query="delete p.*, up.* from produit p left join tbl_uploads up
 	on p.id_produit = up.id_produit
-	where (p.id_produit = '$id_Produit');";
+	where p.id_produit = '_$id_Produit';";
 	$reqExec=mysql_query($query);
 	if(!$reqExec){
 		echo "echec de suppression de produit, ".mysql_error();
@@ -16,13 +16,14 @@ $id_Produit=($_POST['index']);
 	else
 	{
 		
-	$fileName_rs= "select file from tbl_uploads where id_produit='$id_Produit'";
+$fileName_rs= "select file from tbl_uploads where id_produit='$id_Produit'";
 	if(!$fileName_rs){
 		echo "Aucun fichier trouvé lié à ce produit !";
 	}else{
 		
-	while($row=mysql_fetch_array($fileName_rs))
+	while($row = mysql_fetch_array($fileName_rs))
 	{
+echo $row['file'];
 		unlink("../file-uploading/uploads/".$row['file']);
 	}
 	}
